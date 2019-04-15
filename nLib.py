b43c27b15f7d7bb80159be6eff21fb6374f7c9c9
Mohamed Selim <dev.m.selim@gmail.com>
2019-04-15 16:23:04 +0200

    Update nLib.py

--- nLib.py
+++ nLib.py
@@ -16,25 +16,16 @@ class nunchuk:
     time.sleep(self.delay)
     return [self.bus.read_byte(0x52) for i in range(6)]
 
-  def extractAccelData(self, data):
-    x = data[2]+(data[5]>>2&3)
-    y = data[3]+(data[5]>>2&3)
-    z = data[4]+(data[5]>>6)
-    x = x-128 if x>127 else x*-1
-    y = y-128 if x>127 else x*-1
-    z = z-128 if x>127 else x*-1
+  def getAccelData(self, data):
+    x = data[2]+(data[5]>>2&3)-128
+    y = data[3]+(data[5]>>2&3)-128
+    z = data[4]+(data[5]>>6)-128
     return (x, y, z)
 
   def getData(self):
     local = self.read()
-    x = local[2]+(local[5]>>2&3)
-    y = local[3]+(local[5]>>2&3)
-    z = local[4]+(local[5]>>6)
-    x = x-128 if x>127 else x*-1
-    y = y-128 if x>127 else x*-1
-    z = z-128 if x>127 else x*-1
     return {
-      "accel": (x, y, z),
+      "accel": (local[2]+(local[5]>>2&3), local[3]+(local[5]>>2&3), local[4]+(local[5]>>6)),
       "btn":   (local[5]&2!=2, local[5]&1!=1),
       "joystk":(local[0],local[1])
     }
