f6fe51052744a789a86c9a3a9e535458334324f2
Mohamed Selim <dev.m.selim@gmail.com>
2019-04-15 15:22:58 +0200

    Change accelerometer data range
    map [0-255] => [-127, 127]

--- nLib.py
+++ nLib.py
@@ -3,7 +3,6 @@ import RPi.GPIO as rpi
 import time
 
 class nunchuk:
-
   def __init__(self,delay = 0.05):
     self.delay = delay
     self.bus = SMBus(1)
@@ -17,12 +16,21 @@ class nunchuk:
     time.sleep(self.delay)
     return [self.bus.read_byte(0x52) for i in range(6)]
 
+  def extractAccelData(data):
+    x = data[2]+(d[5]>>2&3)
+    y = data[3]+(d[5]>>2&3)
+    z = data[4]+(d[5]>>6)
+    x>127? x=x-128 : x*=-1
+    y>127? y=y-128 : y*=-1
+    z>127? z=z-128 : z*=-1
+    return (x, y, z)
+
   def getData(self):
-    d = self.read()
+    local = self.read()
     return {
-      "accel": (d[2]+(d[5]>>2&3), d[3]+(d[5]>>2&3), d[4]+(d[5]>>6)),
-      "btn":   (d[5]&2!=2, d[5]&1!=1),
-      "joystk":(d[0],d[1])
+      "accel": self.extractAccelData(local),
+      "btn":   (local[5]&2!=2, local[5]&1!=1),
+      "joystk":(local[0],local[1])
     }
 
 
